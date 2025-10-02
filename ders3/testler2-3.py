09d62d89b7f417b3ce7952c50b1de166f41dcf21
Barış Mert Tarlacı <baris.merrtt@gmail.com>
2025-10-02 16:06:08 +0300

    1,2. ve 3. dersin pratikleri

---
+++ ders3/testler2-3.py
@@ -0,0 +1,87 @@
+
+# pratik test2-1
+# print("HİÇBİR ŞEY \n ÇALIŞMAZ, SEN \n ÇALIŞMADIKÇA")
+
+
+
+# pratik test2-2
+
+# print("Öğrenmeyi bırakan herkes, yirmi\n ya da seksen yaşında olsun, \n yaşlıdır" , "Henry Ford", sep="                       ")
+
+
+
+
+
+
+
+# # pratik test2-3
+
+# a = input("birinci sayi:")
+# b = input ("ikinci sayı:")
+
+# print("sayıların toplamı:", int(a) + int(b))
+# print("sayıların çarpımı:", int(a) * int(b))
+# print("sayıların farkı:", int(a) - int(b))
+# print("sayıların bölümü:", int(a) / int(b))
+
+
+
+# # pratik test2-4 yüzde hesaplama
+
+# a = int(input("sayı:"))
+# def yuzde(a):
+#     return a / 100
+# print(yuzde(a))
+
+# pratik test2-5 diktörtgen alan hesaplama
+
+# a = int(input("ilk kenar:"))
+# b = int(input("ikinci kenar:"))
+# def alan(a,b):
+#     return a * b
+# print(alan(a,b))
+
+
+# # pratik test3-1 
+# a = str(52)
+
+# print(a[1])
+# print(a[0])
+
+# pratik test3-2
+
+# a = str(521)
+
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[1] + a[2])
+
+
+# pratiktest3-3
+
+a = input("bir sayı girin:")
+b = input("başka bir sayı girin:")
+
+c= str(int(a)) + str(int(b))
+
+print(c)
+
+
+
+# # pratiktest3-4
+
+# celsius = int(input("sıcaklık girin:"))
+
+# fahrenheit = (celsius * 9/5) + 32
+
+# print(fahrenheit)
+
+
+
+
+
+
+
+
+
